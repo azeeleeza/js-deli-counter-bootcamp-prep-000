0f3ceb62b0158b6e3acef2cd2157e8149fcaf8ff
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 07:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function takeANumber(line, name) {
 
 function nowServing(line) {
     if (line.length) {
+    line.shift()
         return "Currently serving " + line[0] + "."
-        line.shift()
     } else {
         return "There is nobody waiting to be served!"
     }
