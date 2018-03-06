b57f0e03f937ce327d12820f36a108a8cb1d650d
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 07:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-function takeANumber(line,name){
-  var position = line.length + 1
+function takeANumber(line, name) {
+    var position = line.length + 1
 
-  return `Welcome, $(name). You are number $(position) in line.`
+    return `Welcome, $(name). You are number $(position) in line.`
 }
 
-function nowServing(line){
-  if(line.length){
-    return "Now Serving" + line[0]
-  } else{
-    return "There is nobody waiting to be served!"
-  }
+function nowServing(line) {
+    if (line.length) {
+        return "Now Serving" + line[0]
+    } else {
+        return "There is nobody waiting to be served!"
+    }
 }
 
-function currentLine(line){
-  var current = "The line is currently: "
-  if (!line.length){
-    return "The line is currently empty."
-  } else{
-    for (i=0;i<line.length;i++){
-      current += i+1 + ". " + line[1] + ", "
+function currentLine(line) {
+    var current = "The line is currently: "
+    if (!line.length) {
+        return "The line is currently empty."
+    } else {
+        for (i = 0; i < line.length; i++) {
+            current += i + 1 + ". " + line[1] + ", "
+        }
     }
-    }
-  }
 }
